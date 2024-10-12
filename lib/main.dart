c898b7420128f0b879c386e0a7f4514283017085
mg3994 <manishgautammg7@yahoo.com>
2024-10-12 09:47:43 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,9 @@ class MyApp extends StatelessWidget {
           //
           // This works for code too, not just values: Most code changes can be
           // tested with just a hot reload.
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.scaffoldbgColor,
+          ),
           colorScheme: ColorScheme.fromSeed(seedColor: AppColors.seedColor),
           useMaterial3: true,
           iconTheme: const IconThemeData(color: AppColors.primaryColor),
